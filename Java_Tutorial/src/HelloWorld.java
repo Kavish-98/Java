fb21aa296f8a6f328539258ec0514200e3d287c2
Kavish-98 <vimansha98@gmail.com>
2025-06-20 01:58:46 +0530

    Initialize method

---
+++ Java_Tutorial/src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+                First();
+    }
+
+    public static void First() {
+        System.out.println("how to work java methods");
+
+    }
+}
